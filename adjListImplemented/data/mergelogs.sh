6b15227d9a482887808c23a1e2aa8977d38c275b
Chris Cohadari <cohadari.chris@gmail.com>
2023-02-09 11:59:23 +0100

    reorganising

--- adjListImplemented/data/mergelogs.sh
+++ adjListImplemented/data/mergelogs.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-cp benchmarks.txt data.txt
+cp benchmarks.txt data1.txt
 
+i=0
 for Mval in 05 10 15 20 30 40 50
 do
   for Nval in 05 10 15
   do 
-      paste data.txt log.M$Mval.N$Nval.txt >> data.txt
-      echo Ho
+      if ((i %2 == 0))
+      then
+	paste --delimiters=, data1.txt log.M$Mval.N$Nval.txt > data2.txt
+      fi
+
+      if ((i % 2 == 1))
+      then
+	paste --delimiters=, data2.txt log.M$Mval.N$Nval.txt > data1.txt
+      fi
+  ((i++))
   done
 done
 
+paste --delimiters=, data1.txt data2.txt > data.txt
+
+rm data1.txt
+rm data2.txt
 
